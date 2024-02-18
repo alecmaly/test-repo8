84c58b8ac5428c17960a07d92e7bf2d834ef9ec7
alec <alecjmaly@gmail.com>
2024-02-18 10:23:40 -0500

    updated

--- src/Counter.sol
+++ src/Counter.sol
@@ -11,4 +11,30 @@ contract Counter {
     function increment() public {
         number++;
     }
+
+
+    uint256 public balance;
+    uint256 public price;
+    uint256 public _totalShares;
+    uint256 public _totalVoidAndRemainders;
+    
+    function init(uint256 _balance, uint256 _price, uint256 totalShares, uint256 void) public {
+        balance = _balance;
+        price = _price;
+        _totalShares = totalShares;
+        _totalVoidAndRemainders = void;
+    }
+
+    function _addValue(uint256) public {
+        uint256 yieldBearingEth = price * _totalShares;
+        uint256 pending = balance - yieldBearingEth - _totalVoidAndRemainders; // @audit _totalVoidAndRemainders not used in other similar funcitons
+        if (pending < _totalShares || _totalShares == 0) {
+            return;
+        }
+
+        price += (pending / _totalShares);
+    }
+
+
+
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -21,4 +21,57 @@ contract CounterTest is Test {
         counter.setNumber(x);
         assertEq(counter.number(), x);
     }
+
+
+    function testFuzz_addValue(uint256 price, uint256 balance, uint256 totalShares, uint256 void) public {
+        if (price == 0 || balance == 0 || totalShares == 0 || void == 0) {
+            return;
+        }
+        
+        uint256 yieldBearingEth = price * totalShares;
+        if(price != 0 && totalShares > type(uint256).max / price) {
+            return; // Exit if multiplication would overflow
+        }
+
+
+        uint256 sum;
+        unchecked {
+            sum = yieldBearingEth + void;
+        }
+        if (sum < yieldBearingEth || sum < void) {
+            return;
+        }
+
+        if (yieldBearingEth > type(uint256).max - void) {
+            return;
+        }
+        
+        if (balance > type(uint256).max - (yieldBearingEth + void)) {
+            return;
+        }
+
+
+        uint256 pending = balance - yieldBearingEth - void; // @audit _totalVoidAndRemainders not used in other similar funcitons
+        if (pending < totalShares || totalShares == 0) {
+            return;
+        }
+
+
+        // guard against price overflow
+        if (price > type(uint256).max - (pending / totalShares)) {
+            return;
+        }
+        // price += (pending / totalShares);
+
+        
+        // counter.init(balance, price, totalShares, void);
+        
+        
+        // counter._addValue(3);
+        // uint256 price_before = counter.price();
+        // counter._addValue(3);
+        // uint256 price_after = counter.price();
+        
+        // assertEq(price_before, price_after);
+    }
 }
